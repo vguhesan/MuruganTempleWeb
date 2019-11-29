e58e6a29bbbea0d7ae1301ee94feb8614c54808d
Venkatt Guhesan <vguhesan@gmail.com>
2019-11-29 00:50:00 -0500

    one more change to JS files.

--- static/js/rsconfig-homepage-only.js
+++ static/js/rsconfig-homepage-only.js
@@ -1,6 +1,7 @@
 var mySwiper;
 var galleryThumbs;
 
+/* USED BY BOTTOM ROLLING GALLERY ON HOMEPAGE */
 function loadSlider() {
     galleryThumbs = new Swiper('.gallery-thumbs1', {
       spaceBetween: 10,
@@ -48,10 +49,12 @@ function loadSlider() {
     //galleryThumbs.controller.control = galleryTop;
 }
 
-var imgPrefixPath1 = '/slider-images/';
+var rndNumSI = Math.floor(Math.random() * 100000);
+var imgPrefixPath1 = '/slider-images/?rnd='+ rndNumSI;
 
 jQuery(document).ready(function() {
     // Fetch all slides from server /slider-images/
+
     var imgFilesDirectory = imgPrefixPath1;
     var str = '';
 
@@ -66,22 +69,12 @@ jQuery(document).ready(function() {
             var jpgUrl = $(this).attr('href');
             var newJpgUrl = jpgUrl.replace(/slider-images/g, 'slider-images');
 
-
-            //console.log(jpgUrl);
-
-            //$('#slidelist').prepend('<img id="' + jpgName + '" src="images/' + jpgUrl + '" />') 
-            //<li class="touchcarousel-item">
-            //<img src="/p/static/img/mainslider3/1.png"  width="400" />
-            //</li>
             str += '<li class="touchcarousel-item"><img src="' + newJpgUrl + '" width="400"/></li>';
 
         });
 
-        //console.log(str);
         $('#carousel1body').prepend(str);
-        //$('#slidelist1bottom').prepend(str) ;
-
-        //loadSlider();
+        
         $("#carousel1").touchCarousel({
             autoplay: true,
             loopItems: true,
@@ -92,17 +85,11 @@ jQuery(document).ready(function() {
 
 });
 
-var bannerHrefLinks = "{}";
+/* ===================================================================== */
 
-// function loadJSONLinksFromFile() {
-//     var rndNum = Math.floor(Math.random() * 100000);
-//     var jsonFile = '/banner-images/bannerlinksfile.json?rnd='+ rndNum;
-//     return $.ajax({url: jsonFile}).then(function(jsonlinks) {
-//         console.log("jsonLinks:");
-//         console.log(jsonlinks);
-//         bannerHrefLinks = jsonlinks;
-//     });
-// }
+/* USED BY TOP BANNERS ON HOME PAGE */
+
+var bannerHrefLinks = "{}";
 
 function loadJSONLinksFromFile() {
     var rndNum = Math.floor(Math.random() * 100000);
@@ -111,7 +98,8 @@ function loadJSONLinksFromFile() {
 }
 
 function fetchImgList() {
-    var imgFilesDirectory = '/banner-images/';
+    var rndNum = Math.floor(Math.random() * 100000);
+    var imgFilesDirectory = '/banner-images/?rnd='+ rndNum;
     return $.ajax({url: imgFilesDirectory});
 }
 
@@ -123,46 +111,30 @@ jQuery(document).ready(function() {
 
     $.when(loadJSONLinksFromFile(), fetchImgList()).done( function(a1, a2) {
         bannerHrefLinks = a1[0];
-        console.log("a1");
-        console.log(a1);
-        console.log("bannerHrefLinks");
-        console.log(bannerHrefLinks);
         // create temporary DOM element
         var document = $(a2[0]);
-        console.log("a2");
-        console.log(a2);
         // find all links ending with .jpg 
         var str = '';
         document.find('a[href$=".jpg"], a[href$=".png"]').each(function() {
-            console.log("pos-B");
             var jpgName = $(this).text();
             var jpgUrl = $(this).attr('href');
             var newJpgUrl = jpgUrl.replace(/banner-images/g, 'banner-images');
             var hrefBeginPortion = '';
             var hrefEndPortion = '';
-            console.log("pos-C");
             $(bannerHrefLinks).each(function( i, val ) {
-                console.log("pos-D");
                 filename = val['filename'];
                 linkUrl = val['link'];
                 target = val['target'];
                 if (jpgUrl.toLowerCase().indexOf(filename.toLowerCase()) > 0) {
-                    console.log("pos-E");
-                    console.log("Found a match on "+ jpgUrl);
                     hrefBeginPortion = "<a href=\"" + linkUrl + "\" target=\"" + target + "\" >";
                     hrefEndPortion = '</a>';
                 }
             });
-
-            //<div class="rsContent">
-            //<img class="rsImg" src="/p/static/img/mainslider1/Brahmotsavam-webv2-1.jpg" alt="Brahmotsavam 2019" />
-            //</div>
             
             str += '<div class="rsContent">' + hrefBeginPortion + '<img class="rsImg" src="' + newJpgUrl + '"/>' + hrefEndPortion + '</div>';
 
         });
 
-        console.log(str);
         $('#sliderone').prepend(str);
 
         $("#sliderone").royalSlider({
