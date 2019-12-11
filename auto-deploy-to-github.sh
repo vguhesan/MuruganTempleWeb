61cd4e154cd0c881dc1cdb68e120f0349a1fc57f
Venkatt Guhesan <vguhesan@gmail.com>
2019-12-10 22:41:15 -0500

    compressed images (using ImageOptim.app)

--- auto-deploy-to-github.sh
+++ auto-deploy-to-github.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
+rm ~/Projects/2save/MTNAv2/WebSites/www/public/banner-images/*
+rm ~/Projects/2save/MTNAv2/WebSites/www/public/slider-images/*
 #cd ~/Projects/2save/MTNAv2/WebSites/www/public
 #git pull
 cd ~/Projects/2save/MTNAv2/WebSites/www
+
 hugo -D
 cd ./public
 tree -H '/banner-images' -L 1 --noreport --charset utf-8 -P "*.jpg|*.png" ./banner-images/ > ./banner-images/index.html
 tree -H '/slider-images' -L 1 --noreport --charset utf-8 -P "*.jpg|*.png" ./slider-images/ > ./slider-images/index.html
 git add .
-git commit -m "latest commit"
+outfile="Latest Site Updated Commit From: $(date +%Y%m%d)"
+git commit -m "${outfile}"
 git push -u origin master
 cd ~/Projects/2save/MTNAv2/WebSites/www
 
