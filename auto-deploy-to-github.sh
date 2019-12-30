59bcb9240a7e4ea3ecc8ccd43b7873ae7f0d7a35
Venkatt Guhesan <vguhesan@gmail.com>
2019-12-30 09:13:49 -0500

    12/29/2019 changes

--- auto-deploy-to-github.sh
+++ auto-deploy-to-github.sh
@@ -4,11 +4,14 @@ rm ~/Projects/2save/MTNAv2/WebSites/www/public/slider-images/*
 #cd ~/Projects/2save/MTNAv2/WebSites/www/public
 #git pull
 cd ~/Projects/2save/MTNAv2/WebSites/www
-
-hugo -D
-cd ./public
+cd ./static
 tree -H '/banner-images' -L 1 --noreport --charset utf-8 -P "*.jpg|*.png" ./banner-images/ > ./banner-images/index.html
 tree -H '/slider-images' -L 1 --noreport --charset utf-8 -P "*.jpg|*.png" ./slider-images/ > ./slider-images/index.html
+cd ..
+hugo -D
+cd ./public
+#tree -H '/banner-images' -L 1 --noreport --charset utf-8 -P "*.jpg|*.png" ./banner-images/ > ./banner-images/index.html
+#tree -H '/slider-images' -L 1 --noreport --charset utf-8 -P "*.jpg|*.png" ./slider-images/ > ./slider-images/index.html
 git add .
 outfile="Latest Site Updated Commit From: $(date +%Y%m%d)"
 git commit -m "${outfile}"
